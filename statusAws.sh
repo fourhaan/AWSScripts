2acfc0d4eae3ab7edc19006a6a7c0b90209ea916
Mirga Farhaan Baig <116679001+fourhaan@users.noreply.github.com>
2025-06-11 15:51:30 +0530

    Add files via upload

---
+++ statusAws.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#####################################
+# Author : Mirga Farhaan Baig
+# Date : 11/06/2025
+# Description : Shows status 
+# of running S3 buckets and
+# EC2 instances.
+#####################################
+
+set -e
+set -o
+set -x
+
+echo "The status of s3 buckets being used: "
+aws s3 ls
+
+echo "The instance id of EC2 instance running right now: "
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
